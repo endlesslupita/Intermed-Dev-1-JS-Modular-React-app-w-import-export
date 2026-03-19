bfb101e560a0c68f336459c3531ea9e96abb7aa9
endlesslupita <endlesslupita@users.noreply.github.com>
2026-03-18 21:23:25 -0700

    Customized content with Business Model Canvas image and link to YouTube

--- react-modularity-assignment/src/components/ContentA.js
+++ react-modularity-assignment/src/components/ContentA.js
@@ -1,10 +1,13 @@
 import { Button } from './SharedComponents';
+import Canvas from '../Business-Model-Canvas.jpg';
+
 
 export function ContentA() {
   return (
-    <section>Learn more about the Lean Startup Approach</h2>
-      <p>Business-Model-Canvas.jpg</p>
-      <Button label="Content A Button" />
+    <section><h1>The Business Model Canvas</h1>
+      <p><img src={Canvas} alt="Business Model Canvas" style={{ maxWidth: '100%' }}/>
+</p>
+      <Button label="Learn More about the Business Model Canvas" href="https://www.youtube.com/watch?v=IP0cUBWTgpY" />
     </section>
   );
 }
